a79ef15282b54e4e5895faed1b75c8b7bd3dc717
pinkwade <121804278+pinkwade@users.noreply.github.com>
2020-11-07 20:20:38 +0000

    Fixes #83

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -50,4 +50,4 @@ public:
 	void addHead(T item) {
 		Node *newNode = new Node(item, pHead);
 
-		if (isEmpty())		// if the list is empty then head is th
+		if (isEmpty())		// if the list is empty then head is thenew node and ta
