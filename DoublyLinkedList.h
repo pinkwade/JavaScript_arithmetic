d0bc87bd527eabc755d701b81e899fb64eb7decb
pinkwade <121804278+pinkwade@users.noreply.github.com>
2021-01-27 12:25:08 +0000

    Update pot

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -133,4 +133,5 @@ public:
 		// This is the data value that we are returning
 		T returnData = pHead->data;
 
-		// If pHead contains something aka points to somethin
+		// If pHead contains something aka points to something
+		if (pHead != NU
