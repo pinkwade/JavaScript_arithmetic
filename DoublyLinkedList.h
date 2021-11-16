068068f5a7f560719eabb0adc9781346ec0bd16f
pinkwade <121804278+pinkwade@users.noreply.github.com>
2021-11-16 16:02:14 +0000

    update copyright information

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -278,4 +278,4 @@ public:
 		}
 		bool operator!=(const Iterator &other) { return (m_ptr == other.m_ptr); }
 		T getValue() { return m_ptr->data; }
-		void setValue
+		void setValue(T item) { m_ptr->data
