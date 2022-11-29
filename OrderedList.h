b4f88cdc9deeefbbff96fd30593811b711f9e88e
pinkwade <121804278+pinkwade@users.noreply.github.com>
2022-11-29 16:40:05 +0000

    new admin tag version

--- OrderedList.h
+++ OrderedList.h
@@ -13,3 +13,24 @@ private:
 	List<T> container;
 
 public:
+
+	OrderedList(int capacity = 500): container(capacity){}
+	~OrderedList() {}
+
+	bool isEmpty() const
+	{
+		return container.isEmpty();
+	}
+
+	int getLength() const
+	{
+		return container.getLength();
+	}
+
+	T remove(unsigned pos)
+	{
+		return container.remove(pos);
+	}
+
+	T retreive(unsigned pos) const
+	{
