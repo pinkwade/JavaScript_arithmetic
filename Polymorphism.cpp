1c3c8dda93e9b2c83fe9f92a587819b9662f0e1a
pinkwade <121804278+pinkwade@users.noreply.github.com>
2022-12-09 18:23:25 +0000

    Silence compiler warning.

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -21,3 +21,23 @@ public:
 		return animalName;
 	}
 };
+
+// ANIMAL CLASS - pure virtual, abstract
+class Animal
+{
+public:
+	Animal() {}
+	virtual ~Animal() { cout << "Theres no more animal;" << endl; }
+	virtual void speak() = 0;
+	virtual void buyCollar(string name) = 0;
+};
+
+// CAT CLASS - derived from Animal class
+class Cat : public Animal
+{
+private:
+	Collar catCollar;
+
+public:
+	Cat() {}
+	~Cat() { cout << "Cat has died" << endl; }
